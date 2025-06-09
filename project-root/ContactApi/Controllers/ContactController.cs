ab7a7dbce3c5c3200b9b8646d5d3130d7f5c74ff
Abhitosh Kumar <kumarabhitosh678@gmail.com>
2025-06-09 18:40:50 +0530

    Auto commit message:

--- project-root/ContactApi/Controllers/ContactController.cs
+++ project-root/ContactApi/Controllers/ContactController.cs
@@ -1,1 +1,33 @@
-controller content
+using Microsoft.AspNetCore.Mvc;
+using ContactApi.Models;
+using ContactApi.Data;
+using System.Threading.Tasks;
+
+namespace ContactApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostContact([FromBody] Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
